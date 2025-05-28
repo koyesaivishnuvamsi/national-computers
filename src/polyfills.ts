de76f9f8a3587efb868654cd7a63ca9d077153c0
koyesaivishnuvamsi <vamsijayanth5@gmail.com>
2025-05-28 14:34:28 -0500

    Add Angular main and polyfills entry files

---
+++ src/polyfills.ts
@@ -0,0 +1,9 @@
+/***************************************************************************************************
+ * Load `$localize` - used for i18n support in Angular.
+ */
+import '@angular/localize/init';
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
